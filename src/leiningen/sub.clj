5da4589f0f1a02cd60ebcb11dd7944354db58784
Phil Hagelberg <technomancy@gmail.com>
2012-02-22 15:54:34 -0800

    Rewrite to support Leiningen 2.

--- src/leiningen/sub.clj
+++ src/leiningen/sub.clj
@@ -1,29 +1,27 @@
 (ns leiningen.sub
-  (:use [leiningen.core :only [apply-task exit read-project
-                               task-not-found]]))
+  (:require [leiningen.core.main :as main]
+            [leiningen.core.project :as project]))
 
-
-(defn run-sub-proj
+(defn apply-task-to-subproject
   [sub-proj-dir task-name args]
   (println "Reading project from" sub-proj-dir)
-  (let [sub-project (read-project (str sub-proj-dir "/project.clj"))]
-    (apply-task task-name sub-project args task-not-found)))
+  (let [sub-project (project/read (str sub-proj-dir "/project.clj"))]
+    (main/apply-task task-name sub-project args)))
 
+(defn run-subproject [task-name args total-result sub]
+  (if (zero? total-result)
+    (let [result (apply-task-to-subproject sub task-name args)]
+      (if (and (integer? result) (pos? result)) result 0))
+    total-result))
 
 (defn sub
   "Run task for all subprojects"
   [project task-name & args]
-  (if (empty? (:sub project))
+  (if-let [subprojects (:sub project)]
+    (reduce (partial run-subproject task-name args)
+            0 (:sub project))
     (println "No subprojects defined. Define with :sub key in project.clj, e.g.
 
       :sub [\"modules/dep1\" \"modules/proj-common\"]
 
-Note: Each sub-project directory should have its own project.clj file")
-    (when-let [code (reduce (fn [a b]
-                              (let [a (if (= 0 a) nil a)
-                                    b (if (= 0 b) nil b)]
-                                (or a b)))
-                            (map #(let [r (run-sub-proj % task-name args)]
-                                    (if (and (integer? r) (pos? r)) r 0))
-                                 (:sub project)))]
-      (exit code))))
+Note: Each sub-project directory should have its own project.clj file")))
--- test/leiningen/test_sub.clj
+++
@@ -1,2 +0,0 @@
-(ns leiningen.test-sub
-  (:use clojure.test))
