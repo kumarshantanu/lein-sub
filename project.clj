6e765d5b4ba5939cac48b54650f0499a305c1171
Phil Hagelberg <technomancy@gmail.com>
2012-02-22 15:54:48 -0800

    Release 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-sub "0.2.0-SNAPSHOT"
+(defproject lein-sub "0.2.0"
   :description "Leiningen Subprojects plugin"
   :url "https://github.com/kumarshantanu/lein-sub"
   :eval-in-leiningen true)
