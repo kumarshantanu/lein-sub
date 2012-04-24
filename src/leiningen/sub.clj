ee6e1209c9b1b36967ed061f5901e5205ebcf2d2
Erick Gonzalez <erick@swellimagination.com>
2012-04-24 16:27:28 +0200

    Support for nested sub projects. If a subproject contains a :sub key as well, further subdirectories will be recursed upon, and so on

--- src/leiningen/sub.clj
+++ src/leiningen/sub.clj
@@ -2,11 +2,16 @@
   (:require [leiningen.core.main :as main]
             [leiningen.core.project :as project]))
 
+(declare sub)
+
 (defn apply-task-to-subproject
   [sub-proj-dir task-name args]
   (println "Reading project from" sub-proj-dir)
-  (let [sub-project (project/read (str sub-proj-dir "/project.clj"))]
-    (main/apply-task task-name sub-project args)))
+  (let [sub-project (project/read (str sub-proj-dir "/project.clj"))
+        nested-subprojects (:sub sub-project)]
+    (if nested-subprojects
+      (apply sub sub-project task-name args)
+      (main/apply-task task-name sub-project args))))
 
 (defn run-subproject [task-name args total-result sub]
   (if (zero? total-result)
@@ -19,7 +24,7 @@
   [project task-name & args]
   (if-let [subprojects (:sub project)]
     (reduce (partial run-subproject task-name args)
-            0 (:sub project))
+            0 subprojects)
     (println "No subprojects defined. Define with :sub key in project.clj, e.g.
 
       :sub [\"modules/dep1\" \"modules/proj-common\"]
