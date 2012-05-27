75560acd5ba1b73f4580ce89e731be59f556e489
Erick Gonzalez <erick@swellimagination.com>
2012-05-27 04:14:28 +0200

    apply tasks to *all* projects in the directory hierarchy, not just the leaf projects, and make this recursive behaviour an option via the :recursive option

--- src/leiningen/sub.clj
+++ src/leiningen/sub.clj
@@ -2,31 +2,49 @@
   (:require [leiningen.core.main :as main]
             [leiningen.core.project :as project]))
 
-(declare sub)
+(declare sub-task)
 
-(defn apply-task-to-subproject
-  [sub-proj-dir task-name args]
+(defn- apply-task-to-subproject
+  [sub-proj-dir sub-args task-name args]
   (println "Reading project from" sub-proj-dir)
   (let [sub-project (project/read (str sub-proj-dir "/project.clj"))
         sub-project (reduce project/apply-middleware
                             sub-project (:middleware sub-project))
         nested-subprojects (map #(str sub-proj-dir "/" %1) (:sub sub-project))]
-    (if-not (empty? nested-subprojects)
-      (apply sub (assoc sub-project :sub nested-subprojects) task-name args)
+    (if (and (some #{:recursive} sub-args)
+             (not (empty? nested-subprojects)))
+      (sub-task (assoc sub-project :sub nested-subprojects) sub-args task-name args)
       (main/apply-task task-name sub-project args))))
 
-(defn run-subproject [task-name args total-result sub]
+(defn- run-subproject [sub-args task-name args total-result sub]
   (if (zero? total-result)
-    (let [result (apply-task-to-subproject sub task-name args)]
+    (let [result (apply-task-to-subproject sub sub-args task-name args)]
       (if (and (integer? result) (pos? result)) result 0))
     total-result))
 
+(defn- sub-task
+  [project sub-args task-name args]
+  (reduce (partial run-subproject sub-args task-name args)
+          0 (:sub project))
+  (if (some #{:recursive} sub-args)
+    (main/apply-task task-name project args)))
+
 (defn sub
-  "Run task for all subprojects"
-  [project task-name & args]
-  (if-let [subprojects (:sub project)]
-    (reduce (partial run-subproject task-name args)
-            0 subprojects)
+  "Run task for all subprojects
+
+USAGE: lein sub [:recursive] task [ARGS...]
+Executes the given task on each sub directory as specified via the :sub
+key in project.clj
+
+If the :recursive option is present, sub projects will also be scanned for
+nested subprojects themselves and so on, and the task will be executed in all
+of the projects in the directory tree, including the top level project"
+  [project & args]
+  (if (:sub project)
+    (let [[sub-args rest]  (split-with #(= (first %1) \:) args)
+          sub-args         (map #(keyword (subs %1 1)) sub-args)
+          [task-name args] rest]
+      (sub-task project sub-args task-name args))
     (println "No subprojects defined. Define with :sub key in project.clj, e.g.
 
       :sub [\"modules/dep1\" \"modules/proj-common\"]
