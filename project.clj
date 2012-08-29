797a9ebd4f4b610fac1446115a454959d26eed23
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-08-30 00:01:22 +0530

    bump version to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-sub "0.2.0"
+(defproject lein-sub "0.2.1"
   :description "Leiningen Subprojects plugin"
   :url "https://github.com/kumarshantanu/lein-sub"
   :eval-in-leiningen true)
