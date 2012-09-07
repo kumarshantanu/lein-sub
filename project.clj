3a075e3f881cfe99a259deed422c35c8f6a327c7
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-09-07 08:48:04 +0530

    bump the version to 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject lein-sub "0.2.1"
+(defproject lein-sub "0.2.2"
   :description "Leiningen Subprojects plugin"
   :url "https://github.com/kumarshantanu/lein-sub"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
